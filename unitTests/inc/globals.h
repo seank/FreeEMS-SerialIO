c619244bc5d2568e22e07bebc85e2789540152b5
Sean Keys <info@powerefi.com>
2012-04-26 14:08:11 -0700

    removed misplaced/misnamed readme.md file

---
+++ unitTests/inc/globals.h
@@ -0,0 +1,15 @@
+/*
+ * globals.h
+ *
+ *  Created on: Apr 24, 2012
+ *      Author: seank
+ */
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+#include "../../inc/SerialIO.h"
+
+IPDS::SerialIO *serialConnection;
+
+#endif /* GLOBALS_H_ */
