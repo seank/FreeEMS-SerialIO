13e888c8e6c4faf95816d5a3061a61bd6c68d3c5
Sean Keys <info@powerefi.com>
2012-04-26 14:08:11 -0700

    removed misplaced/misnamed readme.md file

--- docs/readme.md
+++
@@ -1,2 +0,0 @@
-Diagram courtesy of Fred Cooke.
-http://forum.diyefi.org/viewtopic.php?f=8&t=57&start=520
---
+++ unitTests/inc/asyncUnitTesting.h
@@ -0,0 +1,14 @@
+/*
+ * asyncUnitTesting.h
+ *
+ *  Created on: Apr 24, 2012
+ *      Author: seank
+ */
+
+#ifndef ASYNCUNITTESTING_H_
+#define ASYNCUNITTESTING_H_
+
+
+extern void smPingTest();
+
+#endif /* ASYNCUNITTESTING_H_ */
